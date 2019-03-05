679b429d647f5b5d7646c5cd1cce750e8a93744e
mckaycourt <mckay.court@gmail.com>
2019-03-04 22:25:28 -0700

    Made tweets look fo real

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../CSS/style.css';
 
 const styles = {
     border: 'solid black 1px',
@@ -7,11 +8,31 @@ const styles = {
 };
 
 const Tweet = props => (
-    <div style={styles}>
-        <h2>{props.username}</h2>
-        <p>{props.body}</p>
-        <div>
-            Retweets: {props.retweet} Created at: {props.createdAt}
+    <div className="tweetEntry">
+        <div className="tweetEntry-content">
+            <a className="tweetEntry-account-group" href="[accountURL]">
+                <img className="tweetEntry-avatar" src="http://placekitten.com/200/200"/>
+                <strong className="tweetEntry-fullname">
+                    {props.screenName}
+                </strong>
+                <span className="tweetEntry-username"> @<b>{props.username}</b></span>
+                <span className="tweetEntry-timestamp">- {props.createdAt}</span>
+            </a>
+            <div className="tweetEntry-text-container">
+                {props.body}
+            </div>
+        </div>
+        {
+            props.img &&
+            <div className="optionalMedia">
+                <img className="optionalMedia-img" src="http://placekitten.com/500/400"/>
+            </div>
+        }
+        <div className='icons'>
+            <i className="material-icons icon">dvr</i>
+            <i className="material-icons icon">reply</i>
+            <i className="material-icons icon">trending_up</i>
+            <i className="material-icons icon">next_week</i>
         </div>
     </div>
 );
@@ -21,6 +42,7 @@ Tweet.propTypes = {
     body: PropTypes.string,
     retweet: PropTypes.number,
     createdAt: PropTypes.string,
+    screenName: PropTypes.string,
 };
 
 export default Tweet;
--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -3,17 +3,17 @@ import PropTypes from 'prop-types';
 import Tweet from "./Tweet";
 
 const Tweets = props => (
-    <div>
+    <div className='tweetEntry-tweetHolder'>
         {
             props.data.map((tweet, i) => (
-                tweet[0] && tweet[0].user
-                    ?
+                tweet[0] && tweet[0].user &&
                     <Tweet username={tweet[0].user.name}
                            body={tweet[0].text}
                            retweet={tweet[0].retweet_count}
                            createdAt={tweet[0].created_at}
-                           key={i}/>
-                    : <p key={i}>something went wrong. Received: {tweet}</p>
+                           key={i}
+                           screenName={tweet[0].user.screen_name}
+                    />
             ))
         }
     </div>
