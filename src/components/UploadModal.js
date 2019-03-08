acbace94032042a9264a764a44170959f6267121
Nate Kelley <31867265+natemkelley@users.noreply.github.com>
2019-03-08 14:16:22 -0700

    UploadModal

---
+++ src/components/UploadModal.js
@@ -0,0 +1,58 @@
+import React, {
+    Component
+} from 'react';
+import openSocket from 'socket.io-client';
+import DataTable from 'react-data-table-component';
+import M from 'materialize-css';
+import 'materialize-css/dist/css/materialize.min.css';
+import WebFont from 'webfontloader';
+import '../CSS/UploadModal.css';
+import FloatingModalButton from './FloatingModalButton';
+import Modal1 from './Modal1';
+import Modal2 from './Modal2';
+import UploaderProgressBar from './UploaderProgressBar';
+
+
+
+
+
+//socket stuff
+var SocketIOFileUpload = require('socketio-file-upload');
+
+
+class UploadModal extends Component {
+    constructor(props) {
+        super(props);
+        WebFont.load({
+          google: {
+            families: ['Material Icons', 'sans-serif']
+          }
+        });
+    }
+
+    componentDidMount() {
+        var elems = document.querySelectorAll('.fixed-action-btn');
+        var instances = M.FloatingActionButton.init(elems, {direction:'left'});
+        M.AutoInit();
+
+        const socket = this.props.socket;
+        this.setState({
+            socket 
+        })
+    }
+
+    render() {
+        const socket = this.props.socket;
+        let SocketIOFileUpload = require('socketio-file-upload');
+        let uploader = new SocketIOFileUpload(socket);
+        
+        return (    
+            <div>
+                <FloatingModalButton />
+                <Modal1 socket={socket} uploader={uploader}/>
+            </div>
+        )
+    }
+}
+
+export default UploadModal;
