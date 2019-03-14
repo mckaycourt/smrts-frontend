ab45fd7f2e24d5b5a2f74f0b974aa2426114c2e2
david13ean <david13ean@gmail.com>
2019-03-13 23:28:15 -0600

    play pause, will merge with master

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -4,22 +4,38 @@ import '../CSS/Slider.css'
 class Slider extends React.Component {
     state = {
         value: 20,
+        playPause: false,
     };
+
 
     handleChange = (event, value) => {
         this.setState({value: event.target.value});
     };
 
+    play = () => {
+        this.setState({playPause: false});
+        console.log("play clicked");
+    }
+
+    pause = () => {
+        this.setState({playPause: true});
+        console.log("pause clicked");
+    }
+
     render() {
         return (
-            <div className="card">
+            <div className="card fixed-action-btn">
+                <div className="playPause">
+                    {this.state.playPause && <input type="image" onClick={this.play} className="playPause" src="./img/play.png"/>}
+                    {!this.state.playPause && <input type="image" onClick={this.pause} className="playPause" src="./img/pause.png"/>}
+                </div>
                 <div className="slidecontainer">
                     <input 
                         type="range" 
                         min="1" 
                         max="100" 
                         value={this.state.value} 
-                        className="slider" id="myRange" 
+                        className="scrubber"
                         onChange={this.handleChange}
                     />
                 </div>
